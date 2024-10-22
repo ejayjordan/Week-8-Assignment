7cbc290631e0e57272a598575f3f28ea6db6e44f
jay <emma.jordan@bsu.edu>
2024-10-22 17:00:26 -0400

    router implementation, successful data update

--- week8assignment/src/app/page.tsx
+++ week8assignment/src/app/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import { useState, useEffect } from 'react'
- 
+let dataDisplay = [" ", " ", " "]
+let Names: any[] = [" "]
+
 function dataCollection() {
   const [data, setData] = useState<any[]>([]);
+  const router = useRouter()
  
   useEffect(() => {
     fetch('/data.json')
@@ -12,33 +16,40 @@ function dataCollection() {
         setData(data)
       })
   }, [])
- 
-  if (!data) return <p>No data</p>
 
-   return (
-    <>
-      {data.map((item) => (
-        <Item key={item.id} item={item}/>
-      ))}
-      </>
-  );
+  if(data != null){
+    Names = Object.getOwnPropertyNames(data)
+  }
 
-  function Item({item} : {item:any}) {
-    const [showInfo, setShowInfo] = useState();
-  
-    const onClickHandle = () => {
-      console.log("click")
-      setShowInfo(showInfo);
-    };
+  const [displayData, setDisplayData] = React.useState([" ", " ", " ",]);
+
+  function returnData(num: number){
+    if(data != null){
+    dataDisplay[0] = data[Names [num] ]["media"]
+    dataDisplay[1] = data[Names [num] ]["genre"]
+    dataDisplay[2] = data[Names [num] ]["year"]
+
+    setDisplayData(dataDisplay)
+    router.refresh()
+  }}
+
   
     return (
+      <>
       <div>
-        <button onClick={onClickHandle}> {item.name} </button>
-        {showInfo && <div>{item.additionalInfo}</div>}
+        <button onClick={() => returnData(0)}>{Names[0]}</button>
+        <button onClick={() => returnData(1)}>{Names[1]}</button>
+        <button onClick={() => returnData(2)}>{Names[2]}</button>
+        <button onClick={() => returnData(3)}>{Names[3]}</button>
+        <button onClick={() => returnData(4)}>{Names[4]}</button>
       </div>
-    );
-  }
- 
 
+      <div>
+      <p>Media: {displayData[0]}</p>
+      <p>Genre: {displayData[1]}</p>
+      <p>Year: {displayData[2]}</p>
+      </div>
+      </>
+    );
 }
 export default dataCollection
