1a317533263b99576d92e2f135adc2291ef02a4e
jay <emma.jordan@bsu.edu>
2024-10-21 21:18:54 -0400

    attempt to use conditionals

--- week8assignment/src/app/page.tsx
+++ week8assignment/src/app/page.tsx
@@ -1,12 +1,12 @@
 'use client'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { useState, useEffect } from 'react'
  
 function dataCollection() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<any[]>([]);
  
   useEffect(() => {
-    fetch('public\data.json')
+    fetch('/data.json')
       .then((res) => res.json())
       .then((data) => {
         setData(data)
@@ -14,11 +14,44 @@ function dataCollection() {
   }, [])
  
   if (!data) return <p>No data</p>
+
+   return (
+    <>
+      {data.map((item) => (
+        <Item key={item.id} item={item}/>
+      ))}
+      </>
+  );
+
+  function Item({item} : {item:any}) {
+    const [showInfo, setShowInfo] = useState(false);
+    const visible = showInfo;
+
+    const Conditional = ({
+      showWhen,
+      children,
+    }: {
+      showWhen: Boolean;
+      children: ReactNode;
+    }) => {
+      if(showWhen) return <>{children}</>;
+      return <></>;
+    };
+  
+    const onClickHandle = () => {
+      <Conditional showWhen={visible}>
+        {showInfo}
+      </Conditional>
+    };
+  
+    return (
+      <div>
+        <button onClick={onClickHandle}> {item.name} </button>
+        {showInfo && <div>{item.additionalInfo}</div>}
+      </div>
+    );
+  }
  
-  return (
-    <div>
-      {data}
-    </div>
-  )
+
 }
-export default dataCollection;
+export default dataCollection
